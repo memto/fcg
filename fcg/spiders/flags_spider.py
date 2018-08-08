11dffe5fec0e811aa57fc9423edbc9b773eb6a1a
dvn0zzz <dvn0zzz@gmail.com>
2018-08-08 22:59:36 +0700

    add flags_spider and scrape first country flag
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

---
+++ fcg/spiders/flags_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+
+from fcg.items import CountryFlag
+
+
+class FlagsSpider(scrapy.Spider):
+    name = "flags"
+
+    scheme = 'http://'
+
+    start_urls = ['http://flagpedia.net/index']
+
+    def parse(self, response):
+        for link in response.xpath('//td/a')[:1]:
+            yield response.follow(link, self.parse_page)
+
+    def parse_page(self, response):
+        title = response.xpath('//article/h1/text()')[0].extract()
+        image_url = response.xpath(
+            '//article/p[@id=\'flag-detail\']/img/@src').extract_first()
+        image_url = self.scheme + image_url.strip('/')
+
+        main_info = []
+        expected_info = ['Country', 'Capital city',
+                         'Population', 'Total area', 'Code']
+        main_info_selector = response.xpath('//dl')[0]
+        terms = main_info_selector.xpath('dt/text()').extract()
+        descriptions = main_info_selector.xpath('dd/text()').extract()
+        for term, des in zip(terms, descriptions):
+            if term in expected_info:
+                main_info.append((term, des))
+        yield CountryFlag(title=title, image_urls=[image_url], main_info=main_info)

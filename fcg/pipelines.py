ebd4ce1a0ca1da54ab3951bc44178332f3a14102
dvn0zzz <dvn0zzz@gmail.com>
2018-08-23 06:23:39 +0700

    refactor TranslatePipeline
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- fcg/pipelines.py
+++ fcg/pipelines.py
@@ -9,7 +9,7 @@ import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 
-from fcg.helpers.translator import get_translated_by_country_code, get_translated_by_lang
+from fcg.helpers.translator import country_code_to_langs, is_supported_lang, get_translated, get_lang_name
 
 
 class MyImagesPipeline(ImagesPipeline):
@@ -27,17 +27,21 @@ class MyImagesPipeline(ImagesPipeline):
 
 class TranslatePipeline(object):
     def process_item(self, item, spider):
-        print('='*3)
-        try:
-            main_info = item['main_info']
-            country_code = main_info['Code']
-            translations = get_translated_by_country_code(country_code)
-            for translation in translations:
-                print(translation.origin, ' -> ', translation.text)
-        except Exception:
-            translations = get_translated_by_lang('en')
-            for translation in translations:
-                print(translation.origin, ' -> ', translation.text)
+        iso_alpha2_country_code = item['iso_alpha2_country_code']
+        country_code, _ = list(iso_alpha2_country_code.items())[0]
+
+        langs = country_code_to_langs(country_code)
+        country_lang = 'en'
+        for lang in langs:
+            if is_supported_lang(lang):
+                country_lang = lang
+                break
+        lang_name = get_lang_name(country_lang)
+        translations = get_translated(country_lang)
+        translated_texts = [tran.text for tran in translations]
+
+        item['iso_6391_lang_code'] = {country_lang: lang_name}
+        item['translated_texts'] = translated_texts
         
         return item
 

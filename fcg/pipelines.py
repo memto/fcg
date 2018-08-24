9530f4f317945534103f54c16c7f4843b70be4b4
dvn0zzz <dvn0zzz@gmail.com>
2018-08-24 07:02:37 +0700

    update FcgPipeline to generate flashcards
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- fcg/pipelines.py
+++ fcg/pipelines.py
@@ -5,11 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+import subprocess
+import tempfile
+import shutil
+
 import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 
 from fcg.helpers.translator import country_code_to_langs, is_supported_lang, get_translated, get_lang_name
+from fcg.templates import templates
 
 
 class MyImagesPipeline(ImagesPipeline):
@@ -42,10 +48,107 @@ class TranslatePipeline(object):
 
         item['iso_6391_lang_code'] = {country_lang: lang_name}
         item['translated_texts'] = translated_texts
-        
+
         return item
 
 
 class FcgPipeline(object):
+    __page_count = 0
+    __page = None
+    __unziped_templace_dir = os.path.join(
+        tempfile.gettempdir(), 'fcg', 'template')
+    __unziped_templace_pictures_dir = os.path.join(
+        __unziped_templace_dir, 'Pictures')
+    __unziped_template_content_file = os.path.join(
+        __unziped_templace_dir, "content.xml")
+    __unziped_template_content = None
+
+    def open_spider(self, spider):
+        self.project_root = os.path.realpath(spider.settings.get('PROJECT_ROOT'))
+        self.images_store = os.path.realpath(spider.settings.get('IMAGES_STORE'))
+
+        shutil.rmtree(self.__unziped_templace_dir, ignore_errors=True)
+        os.makedirs(self.__unziped_templace_dir, exist_ok=True)
+        template_file = os.path.join(self.project_root, "fcg", "templates", "template.odt")
+        subprocess.run(["unzip", template_file, "-d",
+                        self.__unziped_templace_dir], stdout=subprocess.DEVNULL)
+        with open(self.__unziped_template_content_file) as content_file:
+            self.__unziped_template_content = content_file.read()
+
     def process_item(self, item, spider):
+        image_path = item['image_paths'][0]
+        image_realpath = os.path.join(self.images_store, image_path)
+        shutil.copy(image_realpath, self.__unziped_templace_pictures_dir)
+        image_name = os.path.basename(image_path)
+        front_cel = templates.FRONT_CEL_TMP.format(imageName=image_name)
+
+        iso_alpha2_country_code = item['iso_alpha2_country_code']
+        country_code, country_name = list(iso_alpha2_country_code.items())[0]
+        iso_6391_lang_code = item['iso_6391_lang_code']
+        country_lang, country_lang_name = list(
+            iso_6391_lang_code.items())[0]
+        translated_texts = item['translated_texts']
+        main_info = item['main_info']
+        meta_data = '{}, {}, {}, {} ({}-{}, {}, {})'.format(
+            main_info['Capital city'], main_info['Population'], main_info['Total area'], main_info['GDP'],
+            country_code, country_lang, main_info['Calling code'], main_info['Internet TLD']
+        )
+
+        back_cel_table_name = 'Table{}'.format(country_code)
+        header = '{} - {}'.format(country_name, country_lang_name)
+        back_cel = templates.BACK_CEL_TMP.format(backCelTableName=back_cel_table_name,
+                                                 header=header,
+                                                 text0=translated_texts[0],
+                                                 text1=translated_texts[1],
+                                                 text2=translated_texts[2],
+                                                 text3=translated_texts[3],
+                                                 text4=translated_texts[4],
+                                                 text5=translated_texts[5],
+                                                 text6=translated_texts[6],
+                                                 text7=translated_texts[7],
+                                                 text8=translated_texts[8],
+                                                 text9=translated_texts[9],
+                                                 text10=translated_texts[10],
+                                                 text11=translated_texts[11],
+                                                 footer=meta_data
+                                                 )
+
+        if not self.__page:
+            page_front_table_name = 'FontTable{}'.format(self.__page_count)
+            page_back_table_name = 'BackTable{}'.format(self.__page_count)
+            self.__page = templates.PAGE_TMP_PLACEHOLDER.format(
+                frontTableName=page_front_table_name, backTableName=page_back_table_name)
+            self.__page_count += 1
+
+        if self.__page:
+            place_holder_count = self.__page.count(
+                templates.FRONT_CEL_PLACEHOLDER)
+            if place_holder_count:
+                self.__page = self.__page.replace(
+                    templates.FRONT_CEL_PLACEHOLDER, front_cel, 1)
+                self.__page = self.__page.replace(
+                    templates.BACK_CEL_PLACEHOLDER, back_cel, 1)
+                place_holder_count -= 1
+
+            if place_holder_count == 0:
+                self.__unziped_template_content = self.__unziped_template_content.replace(
+                    templates.PAGE_PLACEHOLDER, self.__page + templates.PAGE_PLACEHOLDER)
+                with open(self.__unziped_template_content_file, 'w') as content_file:
+                    content_file.write(self.__unziped_template_content)
+                self.__page = None
+
         return item
+
+    def close_spider(self, spider):
+        if self.__page:
+            self.__unziped_template_content = self.__unziped_template_content.replace(
+                templates.PAGE_PLACEHOLDER, self.__page + templates.PAGE_PLACEHOLDER)
+            with open(self.__unziped_template_content_file, 'w') as content_file:
+                content_file.write(self.__unziped_template_content)
+
+        cwd = os.getcwd()
+        os.chdir(self.__unziped_templace_dir)
+        output_file = os.path.join(self.project_root, "output", "output.odt")
+        subprocess.run('zip -r {} *'.format(output_file),
+                       shell=True, stdout=subprocess.DEVNULL)
+        os.chdir(cwd)

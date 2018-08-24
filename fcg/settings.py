26e150cad3fb487fbb36902d2079e190d28686bd
dvn0zzz <dvn0zzz@gmail.com>
2018-08-24 07:31:22 +0700

    update settings CONCURRENT_REQUESTS, DOWNLOAD_DELAY due to googletrans limit disable FeedExport
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- fcg/settings.py
+++ fcg/settings.py
@@ -25,12 +25,12 @@ NEWSPIDER_MODULE = 'fcg.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -98,8 +98,8 @@ IMAGES_STORE = os.path.join(PROJECT_ROOT, 'output', 'images')
 
 
 # FEED_URI = 'stdout:'
-FEED_URI = 'file://' + \
-    os.path.join(PROJECT_ROOT, 'output', 'iso_alpha2_country_code.jl')
-FEED_FORMAT = 'jsonlines'
-FEED_EXPORT_FIELDS = ['iso_alpha2_country_code']
-FEED_EXPORT_INDENT = 2
+# FEED_URI = 'file://' + \
+#     os.path.join(PROJECT_ROOT, 'output', 'iso_alpha2_country_code.jl')
+# FEED_FORMAT = 'jsonlines'
+# FEED_EXPORT_FIELDS = ['iso_alpha2_country_code']
+# FEED_EXPORT_INDENT = 2

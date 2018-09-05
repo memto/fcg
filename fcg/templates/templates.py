e1019e2332c035e588e53e507359b21edef6bef0
dvn0zzz <dvn0zzz@gmail.com>
2018-09-05 08:00:52 +0700

    update template
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- fcg/templates/templates.py
+++ fcg/templates/templates.py
@@ -1,31 +1,31 @@
 PAGE_PLACEHOLDER = """<!-- PAGE_PLACEHOLDER -->"""
 CARD_CEL_PLACEHOLDER_MARKER = """CARD_CEL_PLACEHOLDER"""
-CARD_CEL_PLACEHOLDER_FMT = """<!-- {}{}-->""".format(
-    CARD_CEL_PLACEHOLDER_MARKER, '{cardIndex} ')
+CARD_CEL_PLACEHOLDER_FMT = """<!-- {}{} -->""".format(
+    CARD_CEL_PLACEHOLDER_MARKER, '{cardIndex}')
 
 PAGE_TMP_PLACEHOLDER = """         <table:table table:name="{frontTableName}" table:style-name="Table1">
             <table:table-column table:style-name="Table1.A" />
             <table:table-column table:style-name="Table1.B" />
             <table:table-row table:style-name="Table1.1">
                <table:table-cell table:style-name="Table1.A1" office:value-type="string">
-                  <text:p text:style-name="Table_20_Contents">
+                  <text:p text:style-name="P0">
                      <!-- CARD_CEL_PLACEHOLDER4 -->
                   </text:p>
                </table:table-cell>
                <table:table-cell table:style-name="Table1.B1" office:value-type="string">
-                  <text:p text:style-name="Table_20_Contents">
+                  <text:p text:style-name="P0">
                      <!-- CARD_CEL_PLACEHOLDER3 -->
                   </text:p>
                </table:table-cell>
             </table:table-row>
             <table:table-row table:style-name="Table1.1">
                <table:table-cell table:style-name="Table1.A2" office:value-type="string">
-                  <text:p text:style-name="Table_20_Contents">
+                  <text:p text:style-name="P0">
                      <!-- CARD_CEL_PLACEHOLDER2 -->
                   </text:p>
                </table:table-cell>
                <table:table-cell table:style-name="Table1.B2" office:value-type="string">
-                  <text:p text:style-name="Table_20_Contents">
+                  <text:p text:style-name="P0">
                      <!-- CARD_CEL_PLACEHOLDER1 -->
                   </text:p>
                </table:table-cell>
@@ -60,7 +60,7 @@ PAGE_TMP_PLACEHOLDER = """         <table:table table:name="{frontTableName}" ta
 
 """
 
-FRONT_CEL_TMP = """                     <draw:frame draw:style-name="fr1" draw:name="{imageName}" text:anchor-type="as-char" svg:width="5.5in" svg:height="4in" draw:z-index="0">
+FRONT_CEL_TMP = """                     <draw:frame draw:style-name="fr1" draw:name="{imageName}" text:anchor-type="as-char" svg:width="5.1in" svg:height="3.6in" draw:z-index="0">
                         <draw:image xlink:href="Pictures/{imageName}" xlink:type="simple" xlink:show="embed" xlink:actuate="onLoad" />
                      </draw:frame>"""
 
@@ -69,66 +69,62 @@ BACK_CEL_TMP = """                  <table:table table:name="Table3" table:style
                      <table:table-column table:style-name="Table3.B" />
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A1" table:number-columns-spanned="2" office:value-type="string">
-                           <text:p text:style-name="P4">{header}</text:p>
-                           <text:p text:style-name="P5">
-                              <text:span text:style-name="T1">(</text:span>
-                              by Dad with &lt;3
-                              <text:span text:style-name="T2">)</text:span>
-                           </text:p>
+                           <text:p text:style-name="P1">{header}</text:p>
+                           <text:p text:style-name="P2">(by Dad with &lt;3)   </text:p>
                         </table:table-cell>
                         <table:covered-table-cell />
                      </table:table-row>
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text0}</text:p>
+                           <text:p text:style-name="P3">{text0}</text:p>
                         </table:table-cell>
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text1}</text:p>
-                        </table:table-cell>
-                     </table:table-row>
-                     <table:table-row table:style-name="Table3.1">
-                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text2}</text:p>
-                        </table:table-cell>
-                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text3}</text:p>
+                           <text:p text:style-name="P3">{text1}</text:p>
                         </table:table-cell>
                      </table:table-row>
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text4}</text:p>
+                           <text:p text:style-name="P3">{text2}</text:p>
                         </table:table-cell>
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text5}</text:p>
-                        </table:table-cell>
-                     </table:table-row>
-                     <table:table-row table:style-name="Table3.1">
-                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text6}</text:p>
-                        </table:table-cell>
-                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text7}</text:p>
+                           <text:p text:style-name="P3">{text3}</text:p>
                         </table:table-cell>
                      </table:table-row>
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text8}</text:p>
+                           <text:p text:style-name="P3">{text4}</text:p>
                         </table:table-cell>
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text9}</text:p>
+                           <text:p text:style-name="P3">{text5}</text:p>
                         </table:table-cell>
                      </table:table-row>
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text10}</text:p>
+                           <text:p text:style-name="P3">{text6}</text:p>
                         </table:table-cell>
                         <table:table-cell table:style-name="Table3.A2" office:value-type="string">
-                           <text:p text:style-name="P2">{text11}</text:p>
+                           <text:p text:style-name="P3">{text7}</text:p>
+                        </table:table-cell>
+                     </table:table-row>
+                     <table:table-row table:style-name="Table3.1">
+                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
+                           <text:p text:style-name="P3">{text8}</text:p>
+                        </table:table-cell>
+                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
+                           <text:p text:style-name="P3">{text9}</text:p>
+                        </table:table-cell>
+                     </table:table-row>
+                     <table:table-row table:style-name="Table3.1">
+                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
+                           <text:p text:style-name="P3">{text10}</text:p>
+                        </table:table-cell>
+                        <table:table-cell table:style-name="Table3.A2" office:value-type="string">
+                           <text:p text:style-name="P3">{text11}</text:p>
                         </table:table-cell>
                      </table:table-row>
                      <table:table-row table:style-name="Table3.1">
                         <table:table-cell table:style-name="Table3.A8" table:number-columns-spanned="2" office:value-type="string">
-                           <text:p text:style-name="P1">{footer}</text:p>
+                           <text:p text:style-name="P4">{footer}</text:p>
                         </table:table-cell>
                         <table:covered-table-cell />
                      </table:table-row>

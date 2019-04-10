02cca8e08f591f6477c47b8eca31d461d2b287fb
dvn0zzz <dvn0zzz@gmail.com>
2019-04-10 23:32:09 +0700

    fix bug
    Signed-off-by: dvn0zzz <dvn0zzz@gmail.com>

--- fcg/spiders/carbrands_spider.py
+++ fcg/spiders/carbrands_spider.py
@@ -9,20 +9,22 @@ class FlagsSpider(scrapy.Spider):
 
 
   def parse(self, response):
-    # for link in response.xpath('//dl[@class=\'counav\']/dd/a'):
-    for link in response.xpath('//dl[@class=\'counav\']/dd/a')[:1]:
+    for link in response.xpath('//dl[@class=\'counav\']/dd/a'):
+    # for link in response.xpath('//dl[@class=\'counav\']/dd/a')[:1]:
       yield response.follow(link, self.parse_country_page)
 
   def parse_country_page(self, response):
     country = response.xpath('//div[@class=\'main-l\']/div[@class=\'title1\']/h1/text()').extract_first()
     country = country.replace(' Car Brands', '')
     # first logo1 dl in main-l div, first a in dd 
-    # for link in response.xpath('//div[@class=\'main-l\']/dl[@class=\'logo1\'][1]/dd/a[1]'):
-    for link in response.xpath('//div[@class=\'main-l\']/dl[@class=\'logo1\'][1]/dd/a[1]')[:4]:
+    for link in response.xpath('//div[@class=\'main-l\']/dl[@class=\'logo1\'][1]/dd/a[1]'):
+    # for link in response.xpath('//div[@class=\'main-l\']/dl[@class=\'logo1\'][1]/dd/a[1]')[:4]:
       yield response.follow(link, self.parse_brand_page, meta={'country': country})
 
   def parse_brand_page(self, response):
     image_url = response.xpath('//div[@class=\'content\']/p[1]/a/@href').extract_first()
+    if image_url == None or image_url[0] == None:
+      image_url = response.xpath('//div[@class=\'content\']/p[1]/img/@src').extract_first()
     info_fields = ["Founded", "Founder", "Headquarters", "Slogan", "Subsidiaries", "Official Site", "Overview"]
     info_sel = response.xpath('//div[@class=\'content\']/table/tbody')
     brand = info_sel.xpath('tr[1]/th/text()').extract_first().replace(' Information', '')
